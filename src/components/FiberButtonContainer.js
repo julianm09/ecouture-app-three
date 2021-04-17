fd6a555a2aef517371c7c204ddfea9d01b769109
Julian <juliantmayes@gmail.com>
2021-04-17 15:47:02 -0700

    fixed regen error

--- src/components/FiberButtonContainer.js
+++ src/components/FiberButtonContainer.js
@@ -23,10 +23,10 @@ export const FiberButtonContainer = ({
     return (
 
         <FiberButtonContainerUI>
-            <FiberButton onClick={()=>setChangePage('cotton')} fiberName={'Cotton Fiber'} fiberExamples={'Organic Cotton, Cotton'} bgcolor={'#36B9C5'} textCol={"white"} borderStyle={'1px solid white'}></FiberButton>
-            <FiberButton onClick={()=>setChangePage('animal')} fiberName={'Animal Fiber'} fiberExamples={'Wool, Leather'} bgcolor={'#E67C20'} textCol={"white"} borderStyle={'1px solid white'}></FiberButton>
-            <FiberButton onClick={()=>setChangePage('plant')} fiberName={'Plant Fiber'} fiberExamples={'Bamboo, Lyocell'} bgcolor={'#51B27E'} textCol={"white"} borderStyle={'1px solid white'}></FiberButton>
-            <FiberButton onClick={()=>setChangePage('synthetic')} fiberName={'Synthetic Fiber'} fiberExamples={'Polyester, Nylon'} bgcolor={'#AF75BD'} textCol={"white"} borderStyle={'1px solid white'}></FiberButton>
+            <FiberButton fiberName={'Cotton Fiber'} fiberExamples={'Organic Cotton, Cotton'} bgcolor={'#36B9C5'} textCol={"white"} borderStyle={'1px solid white'}></FiberButton>
+            <FiberButton fiberName={'Animal Fiber'} fiberExamples={'Wool, Leather'} bgcolor={'#E67C20'} textCol={"white"} borderStyle={'1px solid white'}></FiberButton>
+            <FiberButton fiberName={'Plant Fiber'} fiberExamples={'Bamboo, Lyocell'} bgcolor={'#51B27E'} textCol={"white"} borderStyle={'1px solid white'}></FiberButton>
+            <FiberButton fiberName={'Synthetic Fiber'} fiberExamples={'Polyester, Nylon'} bgcolor={'#AF75BD'} textCol={"white"} borderStyle={'1px solid white'}></FiberButton>
         </FiberButtonContainerUI>
     
 )}
