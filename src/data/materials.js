d7551514eefb429e47bf2750e33396406c259f64
Julian <juliantmayes@gmail.com>
2021-04-21 16:18:39 -0700

    created data files

---
+++ src/data/materials.js
@@ -0,0 +1,31 @@
+const materials = [
+    {
+        'name' : 'Bamboo',
+        'type' : 'Plant',
+        'score' : '7/10',
+        'fact1' : 'Bamboo requires no need for pesticides or fertilizers. Its growth rate requires no need for replanting as new plants grow from the previous bamboos roots.',
+        'fact2' : 'The process of manufacturing bamboo requires the use of strong chemical solvents. These have the potential to harm anyone using the product.',
+        'fact3' : 'The chemicals used in bamboo processing have the potential to harm the environment through water pollution.',
+        'image' : '/bamboo.svg'
+    },
+    {
+        'name' : '',
+        'type' : '',
+        'score' : '',
+        'fact1' : '',
+        'fact2' : '',
+        'fact3' : '',
+        'image' : ''
+    },
+    {
+        'name' : '',
+        'type' : '',
+        'score' : '',
+        'fact1' : '',
+        'fact2' : '',
+        'fact3' : '',
+        'image' : ''
+    },
+
+
+]
---
+++ src/data/shops.js
@@ -0,0 +1,27 @@
+const shops = [
+    {
+        'name' : 'Wildlife Thrift Store',
+        'type' : 'online',
+        'location' : 'vancouver',
+        'about' : 'We are a for-profit business that donates to local, neighborhood charities quarterly. We have been serving the people of Vancouver, with pride, since 1997 a legend here on The Granville Strip. Family-owned operated, we believe in one single, simple concept: by recycling we are not only able to preserve our environment, but we are granted the privilege of giving back to our community. What began supporting The Wildlife Rescue Association now solely supports people-oriented charities with a focus on helping those dealing with Homelessness Mental Health issues. Each charity holds a special place in our heart: Coast Mental Health, The Gathering Place and Covenant House Through your ongoing and generous support, we have been able to donate hundreds of thousands of dollars to these Charities. Thank you Vancouver and to our donors and shoppers everywhere! You are not only helping to preserve our planet, you enable us to recycle give back.',
+        'contact' : '604-682-0381',
+        'address' : '1295 Granville Street Vancouver, BC V6Z 1M5',
+        'website' : 'https://www.wildlifethriftstore.com/',
+        'instagram' : 'https://www.instagram.com/wildlifethriftstore/',
+        'twitter' : 'https://twitter.com/wildlifethrift',
+        'facebook' : 'https://www.facebook.com/TheWildlifeThriftStore'
+    },
+    {
+        'name' : '',
+        'type' : '',
+        'location' : '',
+        'about' : '',
+        'contact' : '',
+        'address' : '',
+        'website' : '',
+        'instagram' : '',
+        'twitter' : '',
+        'facebook' : ''
+    },
+
+]
