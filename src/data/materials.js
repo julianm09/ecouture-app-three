539181663cd0725f01751297393f49cd936d9fe7
Robert Smith <razzira11@gmail.com>
2021-04-25 02:41:47 -0700

    added info to materials.js

--- src/data/materials.js
+++ src/data/materials.js
@@ -1,4 +1,5 @@
 const materials = [
+    // Plant Materials //
     {
         'name' : 'Bamboo',
         'type' : 'Plant',
@@ -9,23 +10,115 @@ const materials = [
         'image' : '/bamboo.svg'
     },
     {
-        'name' : '',
-        'type' : '',
-        'score' : '',
-        'fact1' : '',
-        'fact2' : '',
-        'fact3' : '',
+        'name' : 'Hemp',
+        'type' : 'Plant',
+        'score' : '9/10',
+        'fact1' : 'Hemp clothing is very dirt-repellent and more durable, more absorbent, and warmer than cotton.',
+        'fact2' : 'Hemp is completely biodegradable and is especially suitable for people with allergies.',
+        'fact3' : 'Hemp grows without the use of pesticides or chemical fertilisers.',
         'image' : ''
     },
     {
-        'name' : '',
-        'type' : '',
-        'score' : '',
-        'fact1' : '',
-        'fact2' : '',
-        'fact3' : '',
+        'name' : 'Linen',
+        'type' : 'Plant',
+        'score' : '8/10.',
+        'fact1' : 'Once it is woven into fabric, linen feels cool and fresh and is very tear-resistant.',
+        'fact2' : 'Linen is made from flax fibres, which require few pesticides or fertilisers for cultivation.',
+        'fact3' : 'It is one of the oldest cultivated textile fibres.',
         'image' : ''
     },
-
+    {
+        'name' : 'Lyocell',
+        'type' : 'Plant',
+        'score' : '8/10.',
+        'fact1' : 'It’s produced from the pulp of eucalyptus trees, which don’t  require a lot of water and pesticides.',
+        'fact2' : 'Lyocell is a semi-synthetic, or cellulosic, fabric that has become very popular in the sustainable fashion world.',
+        'fact3' : 'In the case of the TENCEL brand, only sustainably managed forests are used.',
+        'image' : ''
+    },
+    // Synthetic Materials //
+    {
+        'name' : 'Acrylic',
+        'type' : 'Synethetic',
+        'score' : '8/10.',
+        'fact1' : 'Once it is woven into fabric, linen feels cool and fresh and is very tear-resistant.',
+        'fact2' : 'Linen is made from flax fibres, which require few pesticides or fertilisers for cultivation.',
+        'fact3' : 'It is one of the oldest cultivated textile fibres.',
+        'image' : ''
+    },
+    {
+        'name' : 'Polyester',
+        'type' : 'Synethetic',
+        'score' : '8/10.',
+        'fact1' : 'The fibres are very tear-resistant but poor at absorbing liquids, which is especially useful for sportswear.',
+        'fact2' : 'Polyethylene terephthalate (PET) is one of the most common material types for making clothes.',
+        'fact3' : 'PET is often also blended with other materials.',
+        'image' : ''
+    },
+        {
+        'name' : 'Nylon',
+        'type' : 'Synethetic',
+        'score' : '5/10.',
+        'fact1' : 'It takes thousands of years to decompose under cold conditions like in the ocean.',
+        'fact2' : 'Nylon waste ends up in landfills and oceans and pollutes the air, water sources, and entire food chain, threatening human health, wildlife and the planet.',
+        'fact3' : 'Fortunately, nylon can be recycled, recreated, and remolded to become brand new again and create new products without having to use new resources.',
+        'image' : ''
+    },
+    {
+        'name' : 'Acrylic',
+        'type' : 'Synethetic',
+        'score' : '3/10.',
+        'fact1' : 'The very low-cost of acrylic is its main advantage compared to natural fibers',
+        'fact2' : 'Acrylic is made of polyacrylonitrile polymers that may lead to cancers through skin contact, ingestion, or inhalation.',
+        'fact3' : 'As it breaks down, acrylic releases toxic chemicals and greenhouse gases into the environment.',
+        'image' : ''
+    },
+    // Animal Materials //
+    {
+        'name' : 'Sheep Wool',
+        'type' : 'Animal Fiber',
+        'score' : '6/10.',
+        'fact1' : 'It’s long lasting and typically doesn’t require a lot of chemicals during the processing stage.',
+        'fact2' : 'Most animal agriculture has been associated with environmental degradation and land clearing, and this includes operations that raise sheep for wool.',
+        'fact3' : 'Production of wool has been associated with a range of animal welfare and ethical concerns.',
+        'image' : ''
+    },
+    {
+        'name' : 'Fur',
+        'type' : 'Animal Fiber',
+        'score' : '3/10.',
+        'fact1' : 'For the most part, the animals are kept on fur farms in inappropriate conditions. In addition to physical suffering, the animals often exhibit negative behavioural patterns.',
+        'fact2' : 'Additionally, so-called fur animals generally experience agonizing deaths.',
+        'fact3' : 'Regrettably, global demand for real fur has risen sharply in recent years.',
+        'image' : ''
+    },
+    {
+        'name' : 'Leather',
+        'type' : 'Animal Fiber',
+        'score' : '4/10.',
+        'fact1' : 'Contemporary leather production is a poisonous business since industrially mass-produced leather is mostly tanned and treated with toxic chemicals.',
+        'fact2' : 'Leather is not simply a by-product of the meat industry – much of the slaughter worldwide is carried out exclusively on behalf of the leather industry.',
+        'fact3' : 'It’s also easy to overlook leather patches, which are often found on trousers and jackets.',
+        'image' : ''
+    },
+    // Cotton Types (Cotton technically falls under a plant fiber category, but is often categorized on it's own alongside organic cotton.) //
+    {
+        'name' : 'Cotton',
+        'type' : 'Cotton',
+        'score' : '6/10.',
+        'fact1' : 'Cotton is considered the world’s most important raw material for textiles.',
+        'fact2' : 'The seed hairs of the capsule fruit are picked from the cotton plant and then spun into a yarn from which garments are made.',
+        'fact3' : 'Making a t-shirt from regular cotton takes 2,168 gallons of water.',
+        'image' : ''
+    },
+    {
+        'name' : 'Organic Cotton',
+        'type' : 'Cotton',
+        'score' : '8/10.',
+        'fact1' : 'Organic cotton farmers don’t use any pesticides. They use native insects that eat the smaller bugs threatening to destroy organic cotton.',
+        'fact2' : 'Growing organic cotton emits about 46 percent less CO2 than conventional cotton.',
+        'fact3' : 'Organic cotton fibers are longer and stronger than conventional cotton.',
+        'image' : ''
+    },
 
 ]
